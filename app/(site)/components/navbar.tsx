9afc00520ad3cc63358be657ecedf5832bea50bd
MAGESH H <70838644+mageshyt@users.noreply.github.com>
2024-05-23 22:18:22 +0530

    chore: Update navbar link to dashboard in navbar component

--- app/(site)/components/navbar.tsx
+++ app/(site)/components/navbar.tsx
@@ -1,8 +1,10 @@
 import tw from "tailwind-styled-components";
 import Link from "next/link";
 import Logo from "@/components/logo";
+
 import { currentUser } from "@clerk/nextjs/server";
 import { UserButton } from "@clerk/nextjs";
+
 import { MenuIcon } from "lucide-react";
 
 const Navbar = async () => {
@@ -35,7 +37,7 @@ const Navbar = async () => {
 
       <aside className="flex items-center gap-4">
         <Link
-          href={"#"}
+          href={"/dashboard"}
           className="relative inline-flex h-10 overflow-hidden rounded-full p-[2px] focus:outline-none  "
         >
           <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
