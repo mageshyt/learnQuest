5b84dc0b1a60a928b21c170599f51ad278f758c6
MAGESH H <70838644+mageshyt@users.noreply.github.com>
2024-05-25 17:45:23 +0530

    :lipstick: custom icon icons badge added

---
+++ components/global/icon-badge.tsx
@@ -0,0 +1,59 @@
+import { LucideIcon } from "lucide-react";
+import { cva, type VariantProps } from "class-variance-authority";
+import { cn } from "@/lib";
+
+const backgroundVariant = cva("rounded-full flex items-center justify-center", {
+  variants: {
+    variant: {
+      default: "bg-sky-100",
+      success: "bg-emerald-100",
+    },
+
+    size: {
+      default: "p-2 w-12 h-12",
+      sm: "p-1 w-8 h-8",
+    },
+  },
+  defaultVariants: {
+    variant: "default",
+    size: "default",
+  },
+});
+
+const iconVariant = cva("", {
+  variants: {
+    variant: {
+      default: "text-sky-500",
+      success: "text-emerald-500",
+    },
+    size: {
+      default: "w-8 w-8",
+      sm: "h-4 w-4",
+    },
+  },
+  defaultVariants: {
+    variant: "default",
+    size: "default",
+  },
+});
+
+type BackgroundVariantsProps = VariantProps<typeof backgroundVariant>;
+type IconVariantsProps = VariantProps<typeof iconVariant>;
+
+interface IconBadgeProps extends BackgroundVariantsProps, IconVariantsProps {
+  icon: LucideIcon;
+  className?: string;
+}
+
+export const IconBadge: React.FC<IconBadgeProps> = ({
+  icon: Icon,
+  className,
+  size,
+  variant,
+}) => {
+  return (
+    <div className={cn(backgroundVariant({ size, variant }), className)}>
+      <Icon className={iconVariant({ size, variant })} />
+    </div>
+  );
+};
