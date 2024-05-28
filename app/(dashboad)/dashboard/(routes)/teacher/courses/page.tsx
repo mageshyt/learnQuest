dc627c0b532266e05eb0c4085373c1f3da80c21e
MAGESH H <70838644+mageshyt@users.noreply.github.com>
2024-05-28 10:57:29 +0530

    :lipstick: updated course card

--- app/(dashboad)/dashboard/(routes)/teacher/courses/page.tsx
+++ app/(dashboad)/dashboard/(routes)/teacher/courses/page.tsx
@@ -7,6 +7,8 @@ import { auth } from "@clerk/nextjs/server";
 import { db } from "@/lib";
 import { Button } from "@/components/ui/button";
 import ListView from "@/components/global/list-view";
+import Image from "next/image";
+import { Course } from "@prisma/client";
 
 const CoursesPage = async () => {
   const { userId } = auth();
@@ -25,24 +27,39 @@ const CoursesPage = async () => {
     <div className="p-6">
       {/* list of coursed */}
       <h1 className="text-2xl font-medium">Courses</h1>
-      <ListView
-        items={courses}
-        render={(course) => (
-          <div className="flex m-4 gap-4 flex-col">
-            <div>
-              <h2 className="text-lg font-medium">{course.title}</h2>
-              <p className="text-sm text-slate-700 dark:text-muted-foreground">
-                {course.description}
-              </p>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 items-center">
+        <ListView
+          items={courses}
+          render={(course: Course) => (
+            <div className="flex m-4 gap-4 justify-between flex-col">
+              <div>
+                {course.imageUrl && (
+                  <div className="aspect-video relative max-w-lg ">
+                    <Image
+                      alt="course-image"
+                      src={course.imageUrl}
+                      fill
+                      className="rounded-md object-cover"
+                    />
+                  </div>
+                )}
+                <div className="mt-4">
+                  <h2 className="text-sm font-medium">{course.title}</h2>
+                  <p className="text-xs text-slate-700 dark:text-muted-foreground">
+                    {course.description}
+                  </p>
+                </div>
+              </div>
+              <Link href={`/dashboard/teacher/courses/${course.id}`}>
+                <Button variant={"default"} size="sm" className="w-full">
+                  View
+                </Button>
+              </Link>
             </div>
-            <Link href={`/dashboard/teacher/courses/${course.id}`}>
-              <Button variant={"outline"} size="sm">
-                View
-              </Button>
-            </Link>
-          </div>
-        )}
-      />
+          )}
+        />
+      </div>
+
       <Link href="/dashboard/teacher/courses/create">
         <Button>New Course</Button>
       </Link>
