0970294ed80c3f4dc46ed4cc6a639042c130d30b
MAGESH H <70838644+mageshyt@users.noreply.github.com>
2024-06-01 16:37:11 +0530

    :fire:  Remove Prettier configuration files and VSCode settings

---
+++ env.ts
@@ -0,0 +1,18 @@
+import { createEnv } from "@t3-oss/env-nextjs";
+import { z } from "zod";
+
+export const env = createEnv({
+  server: {
+    GEMINI_API_KEY: z.string(),
+    DATABASE_URL: z.string(),
+  },
+  client: {
+    NEXT_PUBLIC_GEMINI_API_KEY: z.string(),
+  },
+  // If you're using Next.js < 13.4.4, you'll need to specify the runtimeEnv manually
+  runtimeEnv: {
+    GEMINI_API_KEY: process.env.GEMINI_API_KEY,
+    DATABASE_URL: process.env.DATABASE_URL,
+    NEXT_PUBLIC_GEMINI_API_KEY: process.env.NEXT_PUBLIC_GEMINI_API_KEY,
+  },
+});
