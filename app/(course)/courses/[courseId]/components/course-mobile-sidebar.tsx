f834a50d204f6e0c16ff82d4bbd9927db979bd88
MAGESH H <70838644+mageshyt@users.noreply.github.com>
2024-07-13 21:49:18 +0530

    :sparkles: Add CourseNavbar component to course layout

---
+++ app/(course)/courses/[courseId]/components/course-mobile-sidebar.tsx
@@ -0,0 +1,35 @@
+import React, { FC } from "react";
+
+import { Chapter, Course, UserProgress } from "@prisma/client";
+
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { Menu } from "lucide-react";
+import { CourseSidebar } from "./course-sidebar";
+
+interface CourseMobileSidebarProps {
+  course: Course & {
+    chapters: (Chapter & {
+      userProgress: UserProgress[] | null;
+    })[];
+  };
+  progressCount: number;
+}
+
+export const CourseMobileSidebar: FC<CourseMobileSidebarProps> = ({
+  course,
+  progressCount,
+}) => {
+  return (
+    <Sheet>
+      <SheetTrigger className="md:hidden pr-4 hover:opacity-75 transition">
+        <Menu />
+      </SheetTrigger>
+      <SheetContent
+        side="left"
+        className="p-0 bg-white w-72 dark:bg-neutral-950"
+      >
+        <CourseSidebar course={course} progressCount={progressCount} />
+      </SheetContent>
+    </Sheet>
+  );
+};
