2127f9943d37692954fa08009eff32ca6f807a1a
MAGESH H <70838644+mageshyt@users.noreply.github.com>
2024-07-17 17:07:39 +0530

    👌 IMPROVE:  Update success variant in banner component
    - Update the success variant in the banner component to use a different background color and text color

--- components/ui/banner.tsx
+++ components/ui/banner.tsx
@@ -10,7 +10,7 @@ const bannerVariants = cva(
     variants: {
       variant: {
         warning: "bg-yellow-200/80 border-yellow-30 text-primary",
-        success: "bg-emerald-200/80 border-emerald-30 text-primary",
+        success: "bg-emerald-700  text-white",
       },
     },
     defaultVariants: {
