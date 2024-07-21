f8b1bc01c5059d39cfbdf928ebb9097924aae877
MAGESH H <70838644+mageshyt@users.noreply.github.com>
2024-07-21 14:42:57 +0530

    Update date formatting and add time ago functionality

--- lib/format.ts
+++ lib/format.ts
@@ -1,4 +1,12 @@
 import { Remarkable } from "remarkable";
+import { format } from "date-fns";
+import TimeAgo from "javascript-time-ago";
+
+// English.
+import en from "javascript-time-ago/locale/en";
+
+TimeAgo.addDefaultLocale(en);
+
 
 export const formatPrice = (price: number) => {
   return new Intl.NumberFormat("en-US", {
@@ -15,3 +23,17 @@ export const markdownToHtml = (markdown: string): string => {
 export const HtmlToText = (html: string): string => {
   return html.replace(/<[^>]*>?/gm, "");
 };
+
+
+export function dateFormat(
+  date: Date | string | number,
+  formatString: string = "dd MMM"
+) {
+  return format(new Date(date), formatString);
+}
+
+export const timeAgo= (date: Date | string | number) => {
+  const timeAgo = new TimeAgo("en-US");
+  return timeAgo.format(new Date(date));
+
+}
