8b387fe2541eb55d1c7ea0eeab00f43ea827b141
MAGESH H <70838644+mageshyt@users.noreply.github.com>
2024-07-31 18:59:16 +0530

    feat: Add quiz page with animated buttons

--- app/(quiz)/quiz/page.tsx
+++ app/(quiz)/quiz/page.tsx
@@ -1,7 +1,15 @@
+import { AnimatedButton } from "@/components/ui/animated-button";
 import React from "react";
 
 const QuizPage = () => {
-  return <div className="p-4">QuizPage</div>;
+  return (
+    <div className="p-4 space-y-4">
+      {/* quiz Game */}
+      <AnimatedButton>Primary</AnimatedButton>
+      <AnimatedButton>Primary Outline</AnimatedButton>
+      {/* footer */}
+    </div>
+  );
 };
 
 export default QuizPage;
