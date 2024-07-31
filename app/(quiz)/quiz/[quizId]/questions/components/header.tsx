21dc6f83ca1a2eb54396d9b23db84e1fc8a6a4f6
MAGESH H <70838644+mageshyt@users.noreply.github.com>
2024-07-31 18:59:05 +0530

    :recycle: Refactor quiz header component and add exit modal functionality

--- app/(quiz)/quiz/[quizId]/questions/components/header.tsx
+++ app/(quiz)/quiz/[quizId]/questions/components/header.tsx
@@ -1,18 +1,22 @@
+"use client";
 import { Progress } from "@/components/ui/progress";
+import { useModal } from "@/hooks/use-modal";
 import { useQuiz } from "@/hooks/use-quiz";
 import { X } from "lucide-react";
 import React from "react";
 
 const Header = () => {
   const { currentQuestionIndex, questions } = useQuiz();
+  const { openModal } = useModal();
+  const value = (currentQuestionIndex / questions.length) * 100;
 
   return (
-    <header className="pt-[20px]">
+    <header className="pt-[20px] flex items-center px-4 gap-x-2">
       <X
-        onClick={() => {}}
+        onClick={() => openModal("exit-model")}
         className="text-slate-500 hover:opacity-75 transition cursor-pointer"
       />
-      <Progress value={currentQuestionIndex} max={questions.length} />
+      <Progress value={value} max={questions.length} />
     </header>
   );
 };
