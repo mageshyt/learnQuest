05ab2687e9905e3839809174c53ae94a8bf1bb63
MAGESH H <70838644+mageshyt@users.noreply.github.com>
2024-07-31 22:34:43 +0530

    :lipstick:  Update loading screen to use min-h-screen for better responsiveness

--- components/global/loading-screen.tsx
+++ components/global/loading-screen.tsx
@@ -3,7 +3,7 @@ import Loader from "./loader";
 
 const LoadingScreen = () => {
   return (
-    <div className="flex items-center justify-center h-full">
+    <div className="flex items-center justify-center min-h-screen">
       <Loader />
     </div>
   );
