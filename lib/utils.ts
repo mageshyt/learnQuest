da8b9391e1b971e24ccb73a1f7dbc3b3e1c3fdf9
MAGESH H <70838644+mageshyt@users.noreply.github.com>
2024-08-01 19:33:49 +0530

    feat: Add playAudio function for playing audio in the app

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,5 +1,8 @@
+import useSound from "use-sound";
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
+import { audioType } from "@/types/typings";
+import { AUDIO_CONSTANTS } from "./constants";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -11,4 +14,15 @@ export function truncate(str: string, length: number) {
 
 export function random<T>(array: T[]): T {
   return array[Math.floor(Math.random() * array.length)];
+}
+
+export function playAudio(type: audioType) {
+  const audio_src = {
+    correct: AUDIO_CONSTANTS.correct,
+    select: AUDIO_CONSTANTS.select,
+    wrong: AUDIO_CONSTANTS.error,
+  };
+  const [play] = useSound(audio_src[type]);
+
+  return play;
 }
