7083fe66605f567717b72b9b49f7a23dd1239c15
MAGESH H <70838644+mageshyt@users.noreply.github.com>
2024-08-03 21:45:31 +0530

    Update QuizPage component with new heading and description

--- app/(dashboad)/dashboard/(routes)/quiz/page.tsx
+++ app/(dashboad)/dashboard/(routes)/quiz/page.tsx
@@ -1,7 +1,19 @@
+import Heading from "@/components/global/heading";
 import React from "react";
 
 const QuizPage = () => {
-  return <div className="p-4">QuizPage</div>;
+  return (
+    <div className="p-4 space-y-4 h-full overflow-y-auto pb-10 container">
+      {/* ---------------------------header--------------------------- */}
+      <div className="mb-6">
+        <Heading
+          title="Test Your Knowledge"
+          isUnderlined={false}
+          description="Challenge yourself with a variety of quizzes and track your progress. Whether you're preparing for exams or just looking to learn something new, our quizzes are designed to help you succeed."
+        />
+      </div>
+    </div>
+  );
 };
 
 export default QuizPage;
