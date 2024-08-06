04f248ae0df3dce85ffe26374f5eb18d87c0e10b
MAGESH H <70838644+mageshyt@users.noreply.github.com>
2024-08-06 22:03:25 +0530

    feat: Add VideoPlayer component for course demos

---
+++ app/(course)/courses/[courseId]/components/demo-player.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import { FC, useState } from "react";
+
+import MuxPlayer from "@mux/mux-player-react";
+
+import { cn } from "@/lib/utils";
+import { Loader2, Lock } from "lucide-react";
+
+interface VideoPlayerProps {
+  playbackId?: string | null;
+  isLocked: boolean;
+}
+
+export const VideoPlayer: FC<VideoPlayerProps> = ({ playbackId, isLocked }) => {
+  if (!playbackId) return null;
+  // ----------------------------------states----------------------------------
+  const [isRead, setIsRead] = useState(false);
+  console.log("VideoPlayer -> isRead", isRead, isLocked);
+
+  return (
+    <div className="relative aspect-video max-w-7xl  mx-auto  bg-transparent rounded-2xl overflow-hidden">
+      {!isRead && !isLocked && (
+        <div className="absolute rounded-2xl h-auto inset-0 flex flex-col items-center justify-center bg-slate-800">
+          <Loader2 className="text-secondary animate-spin " size={32} />
+          <p className="text-sm text-secondary">Loading Video</p>
+        </div>
+      )}
+
+      {isLocked && (
+        <div className="absolute inset-0 flex flex-col items-center justify-center bg-slate-800 gap-2 text-secondary">
+          <Lock className="h-8 w-8" />
+          <p className="text-sm">
+            This video is locked. Please purchase the course to unlock.
+          </p>
+        </div>
+      )}
+
+      {!isLocked && playbackId && (
+        <MuxPlayer
+          playbackId={playbackId}
+          className={cn(!isRead && "hidden", "")}
+          style={{ background: "white" }}
+          onCanPlay={() => setIsRead(true)}
+          autoPlay
+        />
+      )}
+    </div>
+  );
+};
